3d9205098bd3cc644a7f1dff395bd1585a4e7cd3
diguillindev <scontreras.dev@gmail.com>
2024-07-15 09:55:39 -0400

    cambio de inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,14 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full',
+  },
+
+  {
     path: 'inicio',
     loadComponent: () => import('./pagina/inicio/inicio.page').then( m => m.InicioPage)
-  },
+  }
 ];
  
