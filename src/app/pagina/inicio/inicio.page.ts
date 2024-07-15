be9fa629ab8c264e79787caac161edb09ff5ae17
diguillindev <scontreras.dev@gmail.com>
2024-07-15 11:21:21 -0400

    idem

--- src/app/pagina/inicio/inicio.page.ts
+++ src/app/pagina/inicio/inicio.page.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonList, IonLabel, IonThumbnail, IonSearchbar, IonImg } from '@ionic/angular/standalone';
+import { IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonList, IonLabel, IonThumbnail, IonSearchbar, IonImg, IonButton, IonButtons } from '@ionic/angular/standalone';
 import { TraveldbService } from 'src/app/servicios/traveldb.service';
 import { ListaSugerida } from './modelo/trimapDB';
+import { addIcons } from 'ionicons';
+import {addCircleOutline} from 'ionicons/icons'
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
   standalone: true,
-  imports: [IonImg, IonLabel, IonThumbnail, IonList, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, IonSearchbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonButtons, IonButton, IonImg, IonLabel, IonThumbnail, IonList, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, IonSearchbar, CommonModule, FormsModule]
 })
 export class InicioPage implements OnInit {
 
@@ -20,7 +22,11 @@ export class InicioPage implements OnInit {
 
   constructor(
     private servicio: TraveldbService
-  ) { }
+  ) { 
+    addIcons({
+      addCircleOutline
+    })
+  }
 
   ngOnInit() {}
 
